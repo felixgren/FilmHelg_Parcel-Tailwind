b8f2df833bb1eb8f47e0ebc21688c6b8a86d1964
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-02-28 16:28:12 +0100

    Initial commit

---
+++ src/main.mjs
@@ -0,0 +1,10 @@
+import anime from 'animejs/lib/anime.es.js';
+
+// anime({
+//     targets: 'div',
+//     translateX: 600,
+//     rotate: '1turn',
+//     backgroundColor: '#F00',
+//     duration: 10000,
+//     delay: 1000
+//   });
