02a430288b508524eb99bf61ca514bc227699af4
felixgren <33127919+felixgren@users.noreply.github.com>
2021-03-01 00:07:09 +0100

    hacky maybe dunno

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,8 @@ module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
+        require('@fullhuman/postcss-purgecss')({
+            content: ['./*/*.html']
+          })
     ],
  };
